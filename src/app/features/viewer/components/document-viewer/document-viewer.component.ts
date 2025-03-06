5efbefbf372baf5d116dba2245c653000c258b02
Luiz Jarduli <luizjarduli@gmail.com>
2025-03-06 19:28:03 -0300

    feat(viewer): implement document viewer with header and loading functionality

--- src/app/features/viewer/components/document-viewer/document-viewer.component.ts
+++ src/app/features/viewer/components/document-viewer/document-viewer.component.ts
@@ -1,4 +1,17 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  input,
+  InputSignal,
+  NgZone,
+  output,
+  OutputEmitterRef,
+  Signal,
+  viewChild,
+} from '@angular/core';
+import { environment } from '../../../../../environments/environment';
+import WebViewer, { WebViewerInstance } from '@pdftron/webviewer';
 
 @Component({
   selector: 'app-document-viewer',
@@ -6,4 +19,45 @@ import { Component } from '@angular/core';
   templateUrl: './document-viewer.component.html',
   styleUrl: './document-viewer.component.scss',
 })
-export class DocumentViewerComponent {}
+export class DocumentViewerComponent implements AfterViewInit {
+  public wvInstance?: WebViewerInstance;
+
+  public viewerRef: Signal<ElementRef<HTMLDivElement>> =
+    viewChild.required('viewer');
+
+  public document: InputSignal<string | ''> = input('');
+
+  public onDocumentLoaded: OutputEmitterRef<void> = output<void>();
+
+  private readonly licenseKey = environment.licenseKey;
+
+  constructor(private readonly ngZone: NgZone) {}
+
+  public ngAfterViewInit(): void {
+    this.ngZone.runOutsideAngular(() => {
+      WebViewer(
+        {
+          path: 'assets/webviewer/public',
+          initialDoc: this.document(),
+          licenseKey: this.licenseKey,
+        },
+        this.viewerRef().nativeElement
+      ).then((instance) => {
+        this.wvInstance = instance;
+        const { documentViewer } = this.wvInstance.Core;
+
+        instance.UI.setTheme('dark');
+
+        documentViewer.addEventListener(
+          'documentLoaded',
+          () => {
+            this.ngZone.run(() => {
+              this.onDocumentLoaded.emit();
+            });
+          },
+          { once: true }
+        );
+      });
+    });
+  }
+}
--- src/app/features/viewer/pages/viewer-page/viewer-page.component.ts
+++ src/app/features/viewer/pages/viewer-page/viewer-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { DocumentViewerComponent } from '../../components/document-viewer/document-viewer.component';
+import { DocumentHeaderComponent } from '../../components/document-header/document-header.component';
 
 @Component({
   selector: 'app-viewer-page',
-  imports: [DocumentViewerComponent],
+  imports: [DocumentViewerComponent, DocumentHeaderComponent],
   templateUrl: './viewer-page.component.html',
   styleUrl: './viewer-page.component.scss',
 })
-export class ViewerPageComponent {}
+export class ViewerPageComponent {
+  public document: string =
+    'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf';
+
+  public title: string = 'Demo Document';
+
+  public handleDocumentLoaded() {
+    console.log('Document loaded');
+  }
+}
